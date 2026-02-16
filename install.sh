50067ef719c6142f3eb57a52927360c4ad0da029
william <willouchterlony@gmail.com>
2026-02-16 10:18:14 +0000

    Changed the name of a file

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+yay -S vicinae overskride nmgui cursor-clip-git 
+
+sudo pacman -S waybar hyprland hyprpaper hyprlock matugen neovim ghostty qt5ct qt6ct zenity firefox zsh starship rofimoji
