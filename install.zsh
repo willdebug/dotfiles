37bd94bbd4242b5fde0dd061ccc6a813c8cf1f4d
william <willouchterlony@gmail.com>
2026-02-16 10:17:51 +0000

    made some massive changes

--- install.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/zsh 
-
-yay -S vicinae overskride nmgui cursor-clip-git 
-
-sudo pacman -S waybar hyprland hyprpaper hyprlock matugen neovim ghostty qt5ct qt6ct zenity firefox zsh starship 
