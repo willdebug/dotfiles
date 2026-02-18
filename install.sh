185965cae0ef6917ad32aa180f3810c2ac89bd34
william <willouchterlony@gmail.com>
2026-02-18 19:27:37 +0000

    Made some minor changes to install.sh to also add my gtk theming to install

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
-yay -S vicinae overskride nmgui cursor-clip-git 
+yay -S vicinae overskride nmgui cursor-clip-git zenity-gtk3 
 
-sudo pacman -S waybar hyprland hyprpaper hyprlock matugen neovim ghostty qt5ct qt6ct zenity firefox zsh starship rofimoji
+sudo pacman -S waybar hyprland hyprpaper hyprlock matugen neovim ghostty qt5ct qt6ct firefox zsh starship rofimoji adw-gtk-theme
 
 cargo install wallpaper-util
+
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
