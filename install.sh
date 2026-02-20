3e8f35d4fba0897b199eaf5319ca9eb15fe486b2
william <willouchterlony@gmail.com>
2026-02-20 10:12:14 +0000

    Minor changes

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 yay -S vicinae overskride nmgui cursor-clip-git zenity-gtk3 
 
-sudo pacman -S waybar hyprland hyprpaper hyprlock matugen neovim ghostty qt5ct qt6ct firefox zsh starship rofimoji adw-gtk-theme zoxide fd exa
+sudo pacman -S waybar hyprland hyprpaper hyprlock matugen neovim ghostty qt5ct qt6ct firefox zsh starship rofimoji adw-gtk-theme zoxide fd eza
 
 cargo install wallpaper-util
 
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
