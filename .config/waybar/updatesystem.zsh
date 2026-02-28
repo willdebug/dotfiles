1e33ae54606cc67249e389fee96a1fa7a877eb6f
William Ouchterlony <willouchterlony@gmail.com>
2026-02-28 20:31:54 +0000

    Disabled a feature until it can be fixed

--- .config/waybar/updatesystem.zsh
+++ .config/waybar/updatesystem.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-sudo pacman -Syu 
-yay -Syu
+sudo /usr/bin/pacman -Syu --noconfirm
+/usr/bin/yay -Syu --noconfirm
 killall waybar -SIGUSR2 
 notify-send -a "System Updates" "The system has been successfully updated"
