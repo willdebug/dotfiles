56a611b57434391bb62f5aa930eef1e68313b336
William Ouchterlony <willouchterlony@gmail.com>
2026-03-03 19:37:43 +0000

    Added whole new application for audio visualisation

--- .config/waybar/updatesystem.zsh
+++ .config/waybar/updatesystem.zsh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-sudo /usr/bin/pacman -Syu --noconfirm
-/usr/bin/yay -Syu --noconfirm
+/usr/bin/yay --noconfirm
 killall waybar -SIGUSR2 
 notify-send -a "System Updates" "The system has been successfully updated"
