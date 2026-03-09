979d56cfc4a5005c93a87e5b1253897b6574a8fd
William Ouchterlony <willouchterlony@gmail.com>
2026-03-09 21:54:06 +0000

    Minor changes

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 yay -S vicinae overskride nmgui cursor-clip-git zenity-gtk3 airctl python-pywalfox
 
-sudo pacman -S waybar hyprland hyprpaper hyprlock matugen neovim ghostty qt5ct qt6ct firefox zsh starship rofimoji adw-gtk-theme zoxide fd eza  hyprpolkitagent pacman-contrib cava
+sudo pacman -S waybar hyprland hyprpaper hyprlock matugen neovim ghostty qt5ct qt6ct firefox zsh starship rofimoji adw-gtk-theme zoxide fd eza  hyprpolkitagent pacman-contrib cava zen-browser
 
 cargo install wallpaper-util
 
